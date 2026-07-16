246dfa39c3d326e073644c7df65f3c58d7bf3fa3
Aysenurrsoydan <aysenurrsoydan@gmail.com>
2026-07-16 23:23:01 +0300

    Add Day 01 Variables project

---
+++ Variables/Day01-Variables/Day01-Variables/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace AGE_CALCULATOR_PROGRAM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== AGE CALCULATOR PROGRAM ===");
+            Console.WriteLine("------------------------------");
+
+            Console.WriteLine("Please enter your birth year: ");
+            int birtYear = int.Parse(Console.ReadLine());
+
+            int age = 2026 - birtYear;
+            Console.WriteLine("Your age: " + age);
+
+
+        }
+    }
+}
