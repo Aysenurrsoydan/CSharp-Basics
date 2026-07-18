a98b859c405c16ce90da879b511aa442188a96fc
Aysenurrsoydan <aysenurrsoydan@gmail.com>
2026-07-19 02:54:37 +0300

    for and while loop exercises

---
+++ Loops/for loop/for-loop-exercises/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace for_loop_exercises
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("--- Multiplication Table for " + number + " ---");
+
+            for (int i = 1; i <= 10; i++)
+            {
+                int result = number * i;
+                Console.WriteLine(i + " x " + number + " = " + result);
+            }
+        }
+    }
+}
---
+++ Loops/while loop/while-loop-exercise/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace while_loop_exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int sayi = 1;
+            int toplam = 0;
+
+            while (sayi != 0)
+            {
+                Console.WriteLine("Please enter a number: ");
+                sayi = Convert.ToInt32(Console.ReadLine());
+                
+                if (sayi != 0)
+                {
+                    toplam += sayi;
+                }
+            }
+            
+            Console.WriteLine("toplam: " + toplam);
+
+
+
+
+        }
+    }
+}
