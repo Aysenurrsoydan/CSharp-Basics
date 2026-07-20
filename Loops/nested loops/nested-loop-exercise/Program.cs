56f4752a141fb5923d5ac56a1a0289bb62419077
Aysenurrsoydan <aysenurrsoydan@gmail.com>
2026-07-21 01:36:41 +0300

    NestedLoops: pattern exercises

---
+++ Loops/nested loops/nested-loop-exercise/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace nested_loop_exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Enter the number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            
+
+            for (int i = 1; i <= number; i++)
+            {
+                for (int j = 0; j < number - i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int t = 0; t < i; t++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
