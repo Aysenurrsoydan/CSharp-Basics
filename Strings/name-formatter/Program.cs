3b3b33e986da87786354ccca1122d273f17facf3
Aysenurrsoydan <aysenurrsoydan@gmail.com>
2026-07-27 00:42:07 +0300

    In this section, I learned how to use strings in C#. I practiced common string methods and text processing.

---
+++ Strings/name-formatter/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace name_formatter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter your name and surname: ");
+            string text = Console.ReadLine();
+
+            string fullName = Console.ReadLine();
+
+            string trimmedName = fullName.Trim();
+
+            string lowerName = trimmedName.ToLower();
+
+            string[] words = lowerName.Split(' ');
+
+            string firstName = words[0].Substring(0, 1).ToUpper() + words[0].Substring(1);
+
+            string lastName = words[1].Substring(0, 1).ToUpper() + words[1].Substring(1);
+
+            Console.WriteLine(firstName + " " + lastName);
+
+        }
+    }
+}
---
+++ Strings/string-basics/Program.cs
@@ -0,0 +1,96 @@
+﻿namespace string_basics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Use of lenght
+
+            Console.Write("Please enter a text: ");
+            string text = Console.ReadLine();
+            Console.WriteLine(text.Length);
+
+            // Use of ToUpper()
+            
+            Console.WriteLine(text.ToUpper());
+
+            // Use of ToLower()
+
+            Console.WriteLine(text.ToLower());
+
+            // Use of Trim()
+
+            Console.WriteLine(text.Trim());
+
+            // Use of Constains()
+
+            if (text.Contains("a"))
+            {
+                Console.WriteLine("Valid");
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+
+            if (text.Contains("*"))
+            {
+                Console.WriteLine("Valid");
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+
+            // Use of StartsWith()
+
+            Console.WriteLine(text.StartsWith("a"));
+
+            // Use of EndsWith()
+            
+            Console.WriteLine(text.EndsWith("*"));
+
+            //Replace()
+
+            Console.WriteLine(text.Replace("a", "*"));
+
+            // Substring()
+
+            Console.WriteLine(text.Substring(0,3));
+
+            // Split()
+
+            string[] words = text.Split(' ');
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                Console.WriteLine(words[i]);
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
